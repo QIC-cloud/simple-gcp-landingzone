0c3970b55d2d277c9d308cdc5dfb07d24c372f55
AllaeddineEL <a.elareed@gmail.com>
2026-02-04 06:58:50 +0100

    add vm ip output

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "vm_public_ip" {
+  description = "The public IP address of the VM instance"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
+output "vm_private_ip" {
+  description = "The private IP address of the VM instance"
+  value       = google_compute_instance.vm_instance.network_interface[0].network_ip
+}
