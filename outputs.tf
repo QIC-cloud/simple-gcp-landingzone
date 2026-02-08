91fe56275a1c5eee69c17c468f7a28dececefc4d
ibrahim-qic <ialzaid@qiddiya.com>
2026-02-08 14:08:08 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "vm_public_ip" {
-  description = "The public IP address of the VM instance"
-  value       = google_compute_instance.vm_instance[*].network_interface[0].access_config[0].nat_ip
-}
 output "vm_private_ip" {
   description = "The private IP address of the VM instance"
   value       = google_compute_instance.vm_instance[*].network_interface[0].network_ip
